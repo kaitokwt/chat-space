fc95783aa2ca475412ca5221e8c77534906153e5
kaitokwt <vitesthedust@gmail.com>
2019-08-26 18:24:38 +0900

    group_edit_function_create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
   
@@ -8,7 +10,7 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(groups_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -20,10 +22,19 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: "グループを編集しました"
+    else
+      render :edit
+    end
   end
 
   private
-  def groups_params
+  def group_params
     params.require(:group).permit(:name, {:user_ids => []})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
