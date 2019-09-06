192ffc5a103c450c6bbe8ced48a5168122f93ae0
kaitokwt <vitesthedust@gmail.com>
2019-09-06 16:11:53 +0900

    search_user_remove_function

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,4 @@
-$(document).on("turbolinks:load", function() {
+$(function() {
   var search_list = $("#user-search-result");
 
   function appendUser(user) {
@@ -7,6 +7,7 @@ $(document).on("turbolinks:load", function() {
       <p class="chat-group-user__name">${user.name}</p>
       <div class="chat-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
     </div>`;
+
     search_list.append(html);
   }
 
@@ -17,15 +18,17 @@ $(document).on("turbolinks:load", function() {
       <p class='chat-group-user__name'>${user_name}</p>
       <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
     </div>`;
+
     $("#chat-group-users").append(html);
   }
 
-  $(document).on("keyup", "#user-search-field", function(e) {
+  $(document).on("keyup", "#user-search-field", function() {
     var input = $.trim($(this).val());
+    var group_id = $(".chat__group_id").val();
     $.ajax({
       url: "/users",
       type: "GET",
-      data: { keyword: input },
+      data: { keyword: input, group_id: group_id },
       dataType: "json"
     })
       .done(function(users) {
@@ -44,10 +47,19 @@ $(document).on("turbolinks:load", function() {
         alert("ユーザー検索に失敗しました。");
       })
   });
+
   $(document).on('click', '.chat-search-add', function(){
+    $(this).parent().remove();
     var user_id = $(this).attr('data-user-id');
     var user_name = $(this).prev().text();
     addGroupMember(user_id, user_name)
+  });
+
+  $(document).on('click', '.user-search-remove', function(){
+    $(this).parent().remove();
+  });
+
+  $(document).on('submit', '#chat_group', function(e){
   });
 });
 
